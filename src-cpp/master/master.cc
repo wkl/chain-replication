65e73cb5e3a41800dde80bce70a6b6ad1cd5c3ee
Kelong Wang <kelong.wang@stonybrook.edu>
2014-11-02 21:21:13 -0500

    detech and check heartbeat

--- src-cpp/master/master.cc
+++ src-cpp/master/master.cc
@@ -4,15 +4,17 @@
 namespace po = boost::program_options;
 
 std::unique_ptr<Master> master;
+int check_alive_interval;
+int crash_timeout;
 
 void MasterTCPLoop::handle_msg(proto::Message& msg, proto::Address& from_addr) {
   rec_msg_seq++;
   switch (msg.type()) {
     case proto::Message::HEARTBEAT:
       assert(msg.has_heartbeat());
-      LOG(INFO) << "Master received tcp message from " << from_addr.ip() << ":"
-                << from_addr.port() << ", rec_req_seq = " << rec_msg_seq << endl
-                << msg.ShortDebugString() << endl << endl;
+//      LOG(INFO) << "Master received tcp message from " << from_addr.ip() << ":"
+//                << from_addr.port() << ", rec_req_seq = " << rec_msg_seq << endl
+//                << msg.ShortDebugString() << endl << endl;
       master->handle_heartbeat(msg.heartbeat());
       break;
     default:
@@ -22,7 +24,48 @@ void MasterTCPLoop::handle_msg(proto::Message& msg, proto::Address& from_addr) {
   }
 }
 
+Node* BankServerChain::get_node(const proto::Address& addr) {
+  for (auto &node : server_chain_)
+    if (node == addr)
+      return &node;
+
+  LOG(INFO) << "node not found:" << addr.ShortDebugString() << endl;
+  return nullptr;
+}
+
 void Master::handle_heartbeat(const proto::Heartbeat& hb) {
+  BankServerChain& bsc = get_bank_server_chain_by_id(hb.bank_id());
+  Node *node = bsc.get_node(hb.server_addr());
+  if (node && !node->is_crashed())
+    node->set_report_time();
+}
+
+bool Node::become_crashed() {
+  assert(!crashed_);
+  if (!reported_)
+    return false;
+
+  auto now = steady_clock::now();
+  duration<double> time_span =
+      duration_cast<duration<double>>(now - last_report_time_);
+
+  if (time_span.count() > crash_timeout)
+    crashed_ = true;
+
+  return crashed_;
+}
+
+void check_alive() {
+  while (true) {
+    std::this_thread::sleep_for(std::chrono::seconds(check_alive_interval));
+    for (auto &bsc : master->bank_server_chain())
+      for (auto &node : bsc.second.server_chain())
+        if (!node.is_crashed() && node.become_crashed()) {
+          // TODO notify
+          LOG(INFO) << bsc.first << " " << node.addr().ShortDebugString()
+                    << " become crashed!" << endl;
+        }
+  }
 }
 
 int main(int argc, char* argv[]) {
@@ -57,15 +100,47 @@ int main(int argc, char* argv[]) {
 
     master = std::unique_ptr<Master>(new Master());
 
+    // begin test data
+    BankServerChain bsc1;
+    BankServerChain bsc2;
+    BankServerChain bsc3;
+    Node node1("127.0.0.1", 50001);
+    bsc1.append_node(node1);
+    bsc1.set_head(node1);
+    bsc1.set_tail(node1);
+    Node node2("127.0.0.1", 50002);
+    Node node3("127.0.0.1", 50003);
+    bsc2.append_node(node2).append_node(node3);
+    bsc2.set_head(node2);
+    bsc2.set_tail(node3);
+    Node node4("127.0.0.1", 50004);
+    Node node5("127.0.0.1", 50005);
+    Node node6("127.0.0.1", 50006);
+    bsc3.append_node(node4).append_node(node5).append_node(node6);
+    bsc3.set_head(node4);
+    bsc3.set_tail(node6);
+    master->add_bank("bank1", bsc1);
+    master->add_bank("bank2", bsc2);
+    master->add_bank("bank3", bsc3);
+
     MasterTCPLoop tcp_loop(50000);
     std::thread tcp_thread(tcp_loop);
-    tcp_thread.join();
 
-    /* TODO read config file
+    check_alive_interval = 5;
+    crash_timeout = 5;
+    std::thread check_alive_thread(check_alive);
+
+    tcp_thread.join();
+    check_alive_thread.join();
+    // end test data
+
+    /* TODO read data from config file
     if (vm.count("config-file")) {
       MasterTCPLoop tcp_loop(master->addr().port());
       std::thread tcp_thread(tcp_loop);
+      std::thread check_alive_thread(tcp_loop);
       tcp_thread.join();
+      check_alive_thread.join();
     } else {
       LOG(ERROR) << "Please input the config-file path" << endl << endl;
       return 1;
--- src-cpp/master/master.h
+++ src-cpp/master/master.h
@@ -25,10 +25,28 @@ using std::cout;
 using std::endl;
 using std::cerr;
 using std::chrono::steady_clock;
+using std::chrono::duration;
+using std::chrono::duration_cast;
 
 // server information on Master
 class Node {
-  Node() : reported_(false), crashed_(false){};
+ public:
+  Node(string ip, int port) : reported_(false), crashed_(false){
+    addr_.set_ip(ip);
+    addr_.set_port(port);
+  };
+
+  proto::Address& addr() { return addr_; };
+  bool operator==(const proto::Address& addr) {
+    return addr_.ip() == addr.ip() && addr_.port() == addr.port();
+  }
+  void set_report_time() {
+    last_report_time_ = steady_clock::now();
+    reported_ = true;
+  }
+  bool is_crashed() { return crashed_; };
+  bool has_reported() { return reported_; };
+  bool become_crashed();
 
  private:
   proto::Address addr_;
@@ -40,6 +58,25 @@ class Node {
 class BankServerChain {
  public:
   BankServerChain(){};
+  BankServerChain& append_node(Node node) {
+    server_chain_.push_back(node);
+    return *this;
+  }
+  void set_head(Node node) { head_.CopyFrom(node.addr()); }
+  void set_head(string ip, int port) {
+    head_.set_ip(ip);
+    head_.set_port(port);
+  };
+  void set_tail(Node node) { tail_.CopyFrom(node.addr()); }
+  void set_tail(string ip, int port) {
+    tail_.set_ip(ip);
+    tail_.set_port(port);
+  };
+  proto::Address& head() { return head_; };
+  proto::Address& tail() { return tail_; };
+
+  Node* get_node(const proto::Address& addr);
+  list<Node>& server_chain() { return server_chain_; };
 
  private:
   proto::Address head_;
@@ -52,10 +89,22 @@ class Master {
   Master(){};
 
   void handle_heartbeat(const proto::Heartbeat& hb);
+  void add_bank(string bank_id, BankServerChain& bsc) {
+    auto it = bank_server_chain_.insert(std::make_pair(bank_id, bsc));
+    assert(it.second);
+  }
+  BankServerChain& get_bank_server_chain_by_id(string bank_id) {
+    auto it = bank_server_chain_.find(bank_id);
+    assert(it != bank_server_chain_.end());
+    return it->second;
+  }
 
   // getter/setter
   void set_addr(proto::Address addr) { addr_ = addr; };
   proto::Address& addr() { return addr_; };
+  unordered_map<string, BankServerChain>& bank_server_chain() {
+    return bank_server_chain_;
+  };
 
  private:
   proto::Address addr_;
