df839fc47adcec35b2ee86a42a1fc23499139c7f
Kelong Wang <kelong.wang@stonybrook.edu>
2014-11-23 20:08:10 -0500

    reduce number of servers of test1

--- src-cpp/run_case_server.sh
+++ src-cpp/run_case_server.sh
@@ -5,15 +5,21 @@ if [ $1 -eq 1 ]; then
 master/master -c ../config/test$1.json -l ../logs &
 server/server -c ../config/test$1.json -b bank1 -n 1 -l ../logs &
 server/server -c ../config/test$1.json -b bank1 -n 2 -l ../logs &
-server/server -c ../config/test$1.json -b bank1 -n 3 -l ../logs &
 server/server -c ../config/test$1.json -b bank2 -n 1 -l ../logs &
 server/server -c ../config/test$1.json -b bank2 -n 2 -l ../logs &
-server/server -c ../config/test$1.json -b bank2 -n 3 -l ../logs &
 elif [ $1 -eq 2 ]; then
 master/master -c ../config/test$1.json -l ../logs &
 server/server -c ../config/test$1.json -b bank1 -n 1 -l ../logs &
 server/server -c ../config/test$1.json -b bank1 -n 2 -l ../logs &
 server/server -c ../config/test$1.json -b bank1 -n 3 -l ../logs &
+elif [ $1 -eq 3 -o $1 -eq 4 ]; then
+master/master -c ../config/test$1.json -l ../logs &
+server/server -c ../config/test$1.json -b bank1 -n 1 -l ../logs &
+server/server -c ../config/test$1.json -b bank1 -n 2 -l ../logs &
+server/server -c ../config/test$1.json -b bank1 -n 3 -l ../logs &
+server/server -c ../config/test$1.json -b bank2 -n 1 -l ../logs &
+server/server -c ../config/test$1.json -b bank2 -n 2 -l ../logs &
+server/server -c ../config/test$1.json -b bank2 -n 3 -l ../logs &
 fi
 
 #if [ $1 -eq 3 -o $1 -eq 10 -o $1 -eq 12 -o $1 -eq 15 ]; then
