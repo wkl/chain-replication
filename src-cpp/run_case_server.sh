62e9599c15e6a9de7acaaaf568baedbdf64144ca
Kelong Wang <kelong.wang@stonybrook.edu>
2014-11-24 21:08:24 -0500

    add test case 6

--- src-cpp/run_case_server.sh
+++ src-cpp/run_case_server.sh
@@ -29,5 +29,10 @@ server/server -c ../config/test$1.json -b bank2 -n 2 -l ../logs &
 server/server -c ../config/test$1.json -b bank3 -n 1 -l ../logs &
 server/server -c ../config/test$1.json -b bank3 -n 2 -l ../logs &
 server/server -c ../config/test$1.json -b bank3 -n 3 -l ../logs &
+elif [ $1 -eq 6 ]; then
+master/master -c ../config/test$1.json -l ../logs &
+server/server -c ../config/test$1.json -b bank1 -n 1 -l ../logs &
+server/server -c ../config/test$1.json -b bank1 -n 2 -l ../logs &
+server/server -c ../config/test$1.json -b bank1 -n 3 -l ../logs &
 fi
 
