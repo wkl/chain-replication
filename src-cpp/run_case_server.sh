a5748956f9cec3c003ba64912b223c8f2579cdca
Dandan Zheng <zdd.dandanzheng@gmail.com>
2014-11-24 19:25:26 -0500

    modify test6->test5

--- src-cpp/run_case_server.sh
+++ src-cpp/run_case_server.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -x
 
-if [ $1 -eq 1 -o $1 -eq 5 ]; then
+if [ $1 -eq 1 ]; then
 master/master -c ../config/test$1.json -l ../logs &
 server/server -c ../config/test$1.json -b bank1 -n 1 -l ../logs &
 server/server -c ../config/test$1.json -b bank1 -n 2 -l ../logs &
@@ -20,7 +20,7 @@ server/server -c ../config/test$1.json -b bank1 -n 3 -l ../logs &
 server/server -c ../config/test$1.json -b bank2 -n 1 -l ../logs &
 server/server -c ../config/test$1.json -b bank2 -n 2 -l ../logs &
 server/server -c ../config/test$1.json -b bank2 -n 3 -l ../logs &
-elif [ $1 -eq 6 ]; then
+elif [ $1 -eq 5 ]; then
 master/master -c ../config/test$1.json -l ../logs &
 server/server -c ../config/test$1.json -b bank1 -n 1 -l ../logs &
 server/server -c ../config/test$1.json -b bank2 -n 1 -l ../logs &
