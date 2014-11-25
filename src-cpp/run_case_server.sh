a35b9cf3bacdcc093e26be61113884afd9aacf60
Dandan Zheng <zdd.dandanzheng@gmail.com>
2014-11-24 20:36:22 -0500

    run_case_server.sh

--- src-cpp/run_case_server.sh
+++ src-cpp/run_case_server.sh
@@ -31,22 +31,3 @@ server/server -c ../config/test$1.json -b bank3 -n 2 -l ../logs &
 server/server -c ../config/test$1.json -b bank3 -n 3 -l ../logs &
 fi
 
-#if [ $1 -eq 3 -o $1 -eq 10 -o $1 -eq 12 -o $1 -eq 15 ]; then
-#master/master -c ../config/test$1.json -l ../logs &
-#server/server -c ../config/test$1.json -b bank1 -n 1 -l ../logs &
-#server/server -c ../config/test$1.json -b bank1 -n 2 -l ../logs &
-#
-#elif [ $1 -eq 1 -o $1 -eq 2 -o $1 -eq 4  -o $1 -eq 5  -o $1 -eq 7 -o $1 -eq 9 -o $1 -eq 11 -o $1 -eq 13 -o $1 -eq 16 ]; then
-#master/master -c ../config/test$1.json -l ../logs &
-#server/server -c ../config/test$1.json -b bank1 -n 1 -l ../logs &
-#server/server -c ../config/test$1.json -b bank1 -n 2 -l ../logs &
-#server/server -c ../config/test$1.json -b bank1 -n 3 -l ../logs &
-#
-#elif [ $1 -eq 6 -o $1 -eq 8 -o $1 -eq 14 ]; then
-#master/master -c ../config/test$1.json -l ../logs &
-#server/server -c ../config/test$1.json -b bank1 -n 1 -l ../logs &
-#server/server -c ../config/test$1.json -b bank1 -n 2 -l ../logs &
-#server/server -c ../config/test$1.json -b bank1 -n 3 -l ../logs &
-#server/server -c ../config/test$1.json -b bank1 -n 4 -l ../logs &
-
-#fi
