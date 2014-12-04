bd4e4d355e1196f72c68a5ee66508917b679033f
Kelong Wang <kelong.wang@stonybrook.edu>
2014-12-04 03:16:07 -0500

    Read config from correct directory
    Script for phase3 is not modified. Checkout tag 'phase3' to test.

--- src-cpp/run_case_client.sh
+++ src-cpp/run_case_client.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 set -x
 
-client/client -c ../config/test$1.json -l ../logs
+CONFIG_DIR="../config/phase4"
+client/client -c $CONFIG_DIR/test$1.json -l ../logs
 
 echo "Done"
 killall server
--- src-cpp/run_case_server.sh
+++ src-cpp/run_case_server.sh
@@ -1,38 +1,40 @@
 #!/bin/sh
 set -x
 
+CONFIG_DIR="../config/phase4"
+
 if [ $1 -eq 1 ]; then
-master/master -c ../config/test$1.json -l ../logs &
-server/server -c ../config/test$1.json -b bank1 -n 1 -l ../logs &
-server/server -c ../config/test$1.json -b bank1 -n 2 -l ../logs &
-server/server -c ../config/test$1.json -b bank2 -n 1 -l ../logs &
-server/server -c ../config/test$1.json -b bank2 -n 2 -l ../logs &
+master/master -c $CONFIG_DIR/test$1.json -l ../logs &
+server/server -c $CONFIG_DIR/test$1.json -b bank1 -n 1 -l ../logs &
+server/server -c $CONFIG_DIR/test$1.json -b bank1 -n 2 -l ../logs &
+server/server -c $CONFIG_DIR/test$1.json -b bank2 -n 1 -l ../logs &
+server/server -c $CONFIG_DIR/test$1.json -b bank2 -n 2 -l ../logs &
 elif [ $1 -eq 2 ]; then
-master/master -c ../config/test$1.json -l ../logs &
-server/server -c ../config/test$1.json -b bank1 -n 1 -l ../logs &
-server/server -c ../config/test$1.json -b bank1 -n 2 -l ../logs &
-server/server -c ../config/test$1.json -b bank2 -n 1 -l ../logs &
-server/server -c ../config/test$1.json -b bank2 -n 2 -l ../logs &
+master/master -c $CONFIG_DIR/test$1.json -l ../logs &
+server/server -c $CONFIG_DIR/test$1.json -b bank1 -n 1 -l ../logs &
+server/server -c $CONFIG_DIR/test$1.json -b bank1 -n 2 -l ../logs &
+server/server -c $CONFIG_DIR/test$1.json -b bank2 -n 1 -l ../logs &
+server/server -c $CONFIG_DIR/test$1.json -b bank2 -n 2 -l ../logs &
 elif [ $1 -eq 3 -o $1 -eq 4 ]; then
-master/master -c ../config/test$1.json -l ../logs &
-server/server -c ../config/test$1.json -b bank1 -n 1 -l ../logs &
-server/server -c ../config/test$1.json -b bank1 -n 2 -l ../logs &
-server/server -c ../config/test$1.json -b bank1 -n 3 -l ../logs &
-server/server -c ../config/test$1.json -b bank2 -n 1 -l ../logs &
-server/server -c ../config/test$1.json -b bank2 -n 2 -l ../logs &
-server/server -c ../config/test$1.json -b bank2 -n 3 -l ../logs &
+master/master -c $CONFIG_DIR/test$1.json -l ../logs &
+server/server -c $CONFIG_DIR/test$1.json -b bank1 -n 1 -l ../logs &
+server/server -c $CONFIG_DIR/test$1.json -b bank1 -n 2 -l ../logs &
+server/server -c $CONFIG_DIR/test$1.json -b bank1 -n 3 -l ../logs &
+server/server -c $CONFIG_DIR/test$1.json -b bank2 -n 1 -l ../logs &
+server/server -c $CONFIG_DIR/test$1.json -b bank2 -n 2 -l ../logs &
+server/server -c $CONFIG_DIR/test$1.json -b bank2 -n 3 -l ../logs &
 elif [ $1 -eq 5 ]; then
-master/master -c ../config/test$1.json -l ../logs &
-server/server -c ../config/test$1.json -b bank1 -n 1 -l ../logs &
-server/server -c ../config/test$1.json -b bank2 -n 1 -l ../logs &
-server/server -c ../config/test$1.json -b bank2 -n 2 -l ../logs &
-server/server -c ../config/test$1.json -b bank3 -n 1 -l ../logs &
-server/server -c ../config/test$1.json -b bank3 -n 2 -l ../logs &
-server/server -c ../config/test$1.json -b bank3 -n 3 -l ../logs &
+master/master -c $CONFIG_DIR/test$1.json -l ../logs &
+server/server -c $CONFIG_DIR/test$1.json -b bank1 -n 1 -l ../logs &
+server/server -c $CONFIG_DIR/test$1.json -b bank2 -n 1 -l ../logs &
+server/server -c $CONFIG_DIR/test$1.json -b bank2 -n 2 -l ../logs &
+server/server -c $CONFIG_DIR/test$1.json -b bank3 -n 1 -l ../logs &
+server/server -c $CONFIG_DIR/test$1.json -b bank3 -n 2 -l ../logs &
+server/server -c $CONFIG_DIR/test$1.json -b bank3 -n 3 -l ../logs &
 elif [ $1 -eq 6 ]; then
-master/master -c ../config/test$1.json -l ../logs &
-server/server -c ../config/test$1.json -b bank1 -n 1 -l ../logs &
-server/server -c ../config/test$1.json -b bank1 -n 2 -l ../logs &
-server/server -c ../config/test$1.json -b bank1 -n 3 -l ../logs &
+master/master -c $CONFIG_DIR/test$1.json -l ../logs &
+server/server -c $CONFIG_DIR/test$1.json -b bank1 -n 1 -l ../logs &
+server/server -c $CONFIG_DIR/test$1.json -b bank1 -n 2 -l ../logs &
+server/server -c $CONFIG_DIR/test$1.json -b bank1 -n 3 -l ../logs &
 fi
 
